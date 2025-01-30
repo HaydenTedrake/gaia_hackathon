17e9830cf0653d06b126c4de7bca593cb769dac1
Hayden Tedrake <hayden17@mit.edu>
2025-01-29 21:18:49 -0500

    hi

--- map.py
+++ map.py
@@ -1,1 +1,19 @@
-print('hi')
+import pandas as pd
+from astropy.table import Table
+
+# load the data
+# Specify the path to your .fits file
+file_path = 'all_w_rv_kin_3k.fits'
+
+
+tb = Table.read(file_path)
+df = tb.to_pandas()
+
+
+print('done!')
+
+
+
+
+# save the dataframe
+df.to_csv('3k_gaia_kin_data.csv', index = False)
